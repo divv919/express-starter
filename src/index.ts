4256151e1de8c5b63dcc695dc86d9d2d815a14e8
divv919 <divyanshsoni919@gmail.com>
2026-02-13 22:56:39 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import cors from "cors";
+
+let todos: { name: string; status: "done" | "pending" | "ongoing" }[] = [];
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "This is a get route" });
+});
+
+app.get("/get-all-todo", (_, res) => {
+  res.status(200).json({
+    message: "All todos fetched",
+    allTodos: todos,
+  });
+});
+
+app.post("/add-todo", (req, res) => {
+  const { name } = req.body;
+
+  todos = [
+    ...todos,
+    {
+      name: name,
+      status: "pending",
+    },
+  ];
+  res.status(200).json({ message: "Added todo", name });
+});
+
+app.put("/update-todo", (req, res) => {
+  const { name, status } = req.body;
+  todos = todos.map((todo) => {
+    if (todo.name === name) {
+      return { name, status };
+    }
+    return todo;
+  });
+
+  res.status(200).json({
+    message: "Updated todo",
+    todo: {
+      name,
+      status,
+    },
+  });
+});
+
+app.delete("/delete-todo", (req, res) => {
+  const { name } = req.body;
+  todos = todos.filter((todo) => todo.name !== name);
+
+  res.status(200).json({
+    message: "Deleted todo",
+    name,
+  });
+});
+
+app.get("/throw-error", (req, res) => {
+  setTimeout(() => {
+    throw new Error();
+  }, 0);
+});
+
+app.listen(3000, () => {
+  console.log("Server running at 3000 port");
+});
