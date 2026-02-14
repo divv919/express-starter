58a3a5f9f26f11d1daa64944a4c4760282b00733
divv919 <divyanshsoni919@gmail.com>
2026-02-14 23:45:19 +0530

    Update deployment script directory and modify GET route message

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "This is a get route of the staging branch" });
+  res.status(200).json({ message: "This is a get route of the latest branch" });
 });
 
 app.get("/get-all-todo", (_, res) => {
